3d2d5a0e4dcbe5cf0566724a5a416b852dca751d
Wouter Kursten <wouter@retouw.nl>
2021-10-02 11:44:23 +0200

    added find_user_assigned_desktops.ps1

--- find_user_assigned_desktops.ps1
+++ find_user_assigned_desktops.ps1
@@ -3,35 +3,35 @@
     Retreives all machines a user is assigned to
 
     .DESCRIPTION
-    This script uses the Horizon soap api's to query the Horizon database for all machines a user is assigned to.
+    This script uses the Horizon rest api's to query the Horizon database for all machines a user is assigned to.
 
     .EXAMPLE
     .\find_user_assigned_desktops.ps1 -Credential $creds -ConnectionServerFQDN pod2cbr1.loft.lab -UserName "User2"
 
     .PARAMETER Credential
-    Mandatory: Yes
+    Mandatory: No
     Type: PSCredential
     Object with credentials for the connection server with domain\username and password
 
     .PARAMETER ConnectionServerFQDN
-    Mandatory: No
+    Mandatory: Yes
     Default: String
     FQDN of the connection server to connect to
 
     .PARAMETER Username
-    Mandatory: No
+    Mandatory: Yes
     Username of the user to look for
 
     .PARAMETER Domain
-    Mandatory: No
+    Mandatory: Yes
     Domain to look in
 
     .NOTES
     Created by: Wouter Kursten
-    First version: 28-09-2021
+    First version: 02-10-2021
 
     .COMPONENT
-    VMWare PowerCLI
+    Powershell Core
 
 #>
 
@@ -47,12 +47,125 @@ param (
 
     [parameter(Mandatory = $true,
     HelpMessage = "Username of the user to look for.")]
-    [string]$UserName = $false,
+    [string]$User = $false,
 
     [parameter(Mandatory = $true,
     HelpMessage = "DOmain where the user object exists.")]
     [string]$Domain = $false
 )
+
+function Get-HRHeader(){
+    param($accessToken)
+    return @{
+        'Authorization' = 'Bearer ' + $($accessToken.access_token)
+        'Content-Type' = "application/json"
+    }
+}
+function Open-HRConnection(){
+    param(
+        [string] $username,
+        [string] $password,
+        [string] $domain,
+        [string] $url
+    )
+
+    $Credentials = New-Object psobject -Property @{
+        username = $username
+        password = $password
+        domain = $domain
+    }
+
+    return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+}
+
+function Close-HRConnection(){
+    param(
+        $accessToken,
+        $url
+    )
+    return Invoke-RestMethod -Method post -uri "$url/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+}
+
+function Get-HorizonRestData(){
+    [CmdletBinding(DefaultParametersetName='None')] 
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $ServerURL,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Array of ordered hashtables' )]
+        [array] $filters,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Type of filter Options: And, Or' )]
+        [ValidateSet('And','Or')]
+        [string] $Filtertype,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Page size, default = 500' )]
+        [int] $pagesize = 500,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /rest/external/v1/ad-users-or-groups' )]
+        [string] $RestMethod,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /rest/external/v1/ad-users-or-groups' )]
+        [PSCustomObject] $accessToken,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='$True for rest methods that contain pagination and filtering, default = False' )]
+        [switch] $filteringandpagination,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="id",
+        HelpMessage='To be used with single id based queries like /monitor/v1/connection-servers/{id}' )]
+        [string] $id
+    )
+    if($filteringandpagination){
+        if ($filters){
+            $filterhashtable = [ordered]@{}
+            $filterhashtable.add('type',$filtertype)
+            $filterhashtable.filters = @()
+            foreach($filter in $filters){
+                $filterhashtable.filters+=$filter
+            }
+            $filterflat=$filterhashtable | convertto-json -Compress
+            $urlstart= $ServerURL+"/rest/"+$RestMethod+"?filter="+$filterflat+"&page="
+        }
+        else{
+            $urlstart= $ServerURL+"/rest/"+$RestMethod+"?page="
+        }
+        $results = [System.Collections.ArrayList]@()
+        $page = 1
+        $uri = $urlstart+$page+"&size=$pagesize"
+        $response = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+        $response.foreach({$results.add($_)}) | out-null
+        if ($responseheader.HAS_MORE_RECORDS -contains "TRUE") {
+            do {
+                $page++
+                $uri = $urlstart+$page+"&size=$pagesize"
+                $response = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+                $response.foreach({$results.add($_)}) | out-null
+            } until ($responseheader.HAS_MORE_RECORDS -notcontains "TRUE")
+        }
+    }
+    elseif($id){
+        $uri= $ServerURL+"/rest/"+$RestMethod+"/"+$id
+        $results = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+    }
+    else{
+        $uri= $ServerURL+"/rest/"+$RestMethod
+        $results = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+    }
+
+    return $results
+}
 
 if($Credential){
     $creds = $credential
@@ -63,52 +176,38 @@ else{
 
 $ErrorActionPreference = 'Stop'
 
-# Preview info
-if($preview){
-    write-host "Running in preview mode no actions will be taken" -foregroundcolor Magenta
-}
+$username=($creds.username).split("\")[1]
+$domain=($creds.username).split("\")[0]
+$password=$creds.password
 
-# Loading powercli modules
-Import-Module VMware.VimAutomation.HorizonView
-Import-Module VMware.VimAutomation.Core
+$url = "https://$ConnectionServerFQDN"
 
-$hvserver=connect-hvserver $ConnectionServerFQDN -credential $creds
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password) 
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-# --- Get Services for interacting with the Horizon API Service ---
-$Services= $hvServer.ExtensionData
+$accessToken = Open-HRConnection -username $username -password $UnsecurePassword -domain $Domain -url $url
 
-# Search for UserID
-$queryservice=new-object vmware.hv.queryserviceservice
-$defn = New-Object VMware.Hv.QueryDefinition
-$defn.queryentitytype='ADUserOrGroupSummaryView'
+$userfilters = @()
+$userfilter= [ordered]@{}
+$userfilter.add('type','Equals')
+$userfilter.add('name','name')
+$userfilter.add('value',$User)
+$userfilters+=$userfilter
+$domainfilter= [ordered]@{}
+$domainfilter.add('type','Equals')
+$domainfilter.add('name','domain')
+$domainfilter.add('value',$Domain)
+$userfilters+=$domainfilter
 
-# Filter for the correct user & domain
-$filter1 = New-Object VMware.Hv.QueryFilterEquals
-$filter1.membername='base.domain'
-$filter1.value=$Domain
-$filter2 = New-Object VMware.Hv.QueryFilterEquals
-$filter2.membername='base.name'
-$filter2.value=$UserName
-$filter=new-object vmware.hv.QueryFilterAnd
-$filter.filters=@($filter1, $filter2)
-$defn.filter=$filter
+$userobject = Get-HorizonRestData -ServerURL $url -filteringandpagination -Filtertype "And" -filters $userfilters -RestMethod "/external/v1/ad-users-or-groups" -accessToken $accessToken
 
-# Perform the query
-$UserObject = ($queryService.QueryService_Create($Services, $defn)).results
-$Services.QueryService.QueryService_DeleteAll()
+$machinefilters = @()
+$machinefilter= [ordered]@{}
+$machinefilter.add('type','Contains')
+$machinefilter.add('name','user_ids')
+$machinefilter.add('value',($userobject).id)
+$machinefilters+=$machinefilter
+$machines = Get-HorizonRestData -ServerURL $url -filteringandpagination -Filtertype "And" -filters $machinefilters -RestMethod "/inventory/v1/machines" -accessToken $accessToken
 
-
-# Search for machine details
-$queryservice=new-object vmware.hv.queryserviceservice
-$defn = New-Object VMware.Hv.QueryDefinition
-$defn.queryentitytype='MachineDetailsView'
-
-# We need an array for the filter
-
-
-$defn.filter = New-Object VMware.Hv.QueryFilterEquals -Property @{ 'memberName' = 'data.assignedUser'; 'value' = $userobject.id}
-$MachinesObject  = ($queryService.QueryService_Create($Services, $defn)).results
-$Services.QueryService.QueryService_DeleteAll()
-return $MachinesObject
-
+return $machines
 
