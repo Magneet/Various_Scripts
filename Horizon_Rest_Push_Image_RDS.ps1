aabaaa46f5b2ace81ce73fdc80398e930330ab98
Wouter Kursten <wouter@retouw.nl>
2022-02-24 19:13:33 +0100

    24022022

---
+++ Horizon_Rest_Push_Image_RDS.ps1
@@ -0,0 +1,187 @@
+<#
+    .SYNOPSIS
+    Pushes a new Golden Image to a Desktop Pool
+
+    .DESCRIPTION
+    This script uses the Horizon rest api's to push a new golden image to a VMware Horizon Desktop Pool
+
+    .EXAMPLE
+    .\Horizon_Rest_Push_Image.ps1 -ConnectionServerURL https://pod1cbr1.loft.lab -Credentials $creds -vCenterURL "https://pod1vcr1.loft.lab" -DataCenterName "Datacenter_Loft" -baseVMName "W21h1-2021-09-08-15-48" -BaseSnapShotName "Demo Snapshot" -DesktopPoolName "Pod01-Pool02"
+
+    .PARAMETER Credential
+    Mandatory: No
+    Type: PSCredential
+    Object with credentials for the connection server with domain\username and password. If not supplied the script will ask for user and password.
+
+    .PARAMETER ConnectionServerURL
+    Mandatory: Yes
+    Default: String
+    URL of the connection server to connect to
+
+    .PARAMETER vCenterURL
+    Mandatory: Yes
+    Username of the user to look for
+
+    .PARAMETER DataCenterName
+    Mandatory: Yes
+    Domain to look in
+
+    .PARAMETER BaseVMName
+    Mandatory: Yes
+    Domain to look in
+
+    .PARAMETER BaseSnapShotName
+    Mandatory: Yes
+    Domain to look in
+
+    .PARAMETER FarmName
+    Mandatory: Yes
+    Domain to look in
+
+    .PARAMETER StoponError
+    Mandatory: No
+    Boolean to stop on error or not
+
+    .PARAMETER logoff_policy
+    Mandatory: No
+    String FORCE_LOGOFF or WAIT_FOR_LOGOFF to set the logoff policy.
+
+    .PARAMETER Scheduledtime
+    Mandatory: No
+    Time to schedule the image push in [DateTime] format.
+
+    .NOTES
+    Minimum required version: VMware Horizon 8 2012
+    Created by: Wouter Kursten
+    First version: 13-02-2022
+
+    .COMPONENT
+    Powershell Core
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false,
+    HelpMessage='Credential object as domain\username with password' )]
+    [PSCredential] $Credentials,
+
+    [Parameter(Mandatory=$true,  HelpMessage='FQDN of the connectionserver' )]
+    [ValidateNotNullOrEmpty()]
+    [string] $ConnectionServerURL,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "URL of the vCenter to look in i.e. https://vcenter.domain.lab")]
+    [ValidateNotNullOrEmpty()]
+    [string]$vCenterURL,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Datacenter to look in.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DataCenterName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Golden Image VM.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$BaseVMName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Snapshot to use for the Golden Image.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$BaseSnapShotName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the RDS Farm.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$FarmName,
+
+    [parameter(Mandatory = $false,
+    HelpMessage = "True or false for stop on error.")]
+    [ValidateNotNullOrEmpty()]
+    [bool]$StoponError = $true,
+
+    [parameter(Mandatory = $false,
+    HelpMessage = "Use WAIT_FOR_LOGOFF or FORCE_LOGOFF.")]
+    [ValidateSet('WAIT_FOR_LOGOFF','FORCE_LOGOFF', IgnoreCase = $false)]
+    [string]$logoff_policy = "WAIT_FOR_LOGOFF",
+
+    [parameter(Mandatory = $false,
+    HelpMessage = "DateTime object for the moment of scheduling the image push.Defaults to immediately")]
+    [datetime]$Scheduledtime
+)
+if($Credentials){
+    $username=($credentials.username).split("\")[1]
+    $domain=($credentials.username).split("\")[0]
+    $password=$credentials.password
+}
+else{
+    $credentials = Get-Credential
+    $username=($credentials.username).split("\")[1]
+    $domain=($credentials.username).split("\")[0]
+    $password=$credentials.password
+}
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password) 
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+function Get-HRHeader(){
+    param($accessToken)
+    return @{
+        'Authorization' = 'Bearer ' + $($accessToken.access_token)
+        'Content-Type' = "application/json"
+    }
+}
+function Open-HRConnection(){
+    param(
+        [string] $username,
+        [string] $password,
+        [string] $domain,
+        [string] $url
+    )
+
+    $Credentials = New-Object psobject -Property @{
+        username = $username
+        password = $password
+        domain = $domain
+    }
+
+    return invoke-restmethod -Method Post -uri "$ConnectionServerURL/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+}
+function Close-HRConnection(){
+    param(
+        $accessToken,
+        $ConnectionServerURL
+    )
+    return Invoke-RestMethod -Method post -uri "$ConnectionServerURL/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+}
+
+try{
+    $accessToken = Open-HRConnection -username $username -password $UnsecurePassword -domain $Domain -url $ConnectionServerURL
+}
+catch{
+    throw "Error Connecting: $_"
+}
+
+$vCenters = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/monitor/v2/virtual-centers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$vcenterid = ($vCenters | where-object {$_.name -like "*$vCenterURL*"}).id
+$datacenters = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/datacenters?vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$datacenterid = ($datacenters | where-object {$_.name -eq $DataCenterName}).id
+$basevms = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/base-vms?datacenter_id=$datacenterid&filter_incompatible_vms=false&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$basevmid = ($basevms | where-object {$_.name -eq $baseVMName}).id
+$basesnapshots = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/base-snapshots?base_vm_id=$basevmid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$basesnapshotid = ($basesnapshots | where-object {$_.name -eq $BaseSnapShotName}).id
+$farms = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/inventory/v2/farms" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$farmid = ($farms | where-object {$_.name -eq $FarmName}).id
+$startdate = (get-date -UFormat %s)
+$datahashtable = [ordered]@{}
+$datahashtable.add('logoff_policy',$logoff_policy)
+$datahashtable.add('maintenance_mode',"IMMEDIATE")
+if($Scheduledtime){
+    $starttime = get-date $Scheduledtime
+    $epoch = ([DateTimeOffset]$starttime).ToUnixTimeMilliseconds()
+    $datahashtable.add('next_scheduled_time',$epoch)
+}
+$datahashtable.add('parent_vm_id',$basevmid)
+$datahashtable.add('snapshot_id',$basesnapshotid)
+$datahashtable.add('stop_on_first_error',$StoponError)
+$json = $datahashtable | convertto-json
+Invoke-RestMethod -Method Post -uri "$ConnectionServerURL/rest/inventory/v1/farms/$farmid/action/schedule-maintenance" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken) -body $json
---
+++ Horizon_Rest_update_desktop_pool.ps1
@@ -0,0 +1,71 @@
+$url = "https://pod1cbr1.loft.lab"
+
+$credentials=Import-Clixml .\creds.xml
+$username=($credentials.username).split("\")[1]
+$domain=($credentials.username).split("\")[0]
+$password=$credentials.password
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password) 
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+
+function Get-HRHeader(){
+    param($accessToken)
+    return @{
+        'Authorization' = 'Bearer ' + $($accessToken.access_token)
+        'Content-Type' = "application/json"
+    }
+}
+function Open-HRConnection(){
+    param(
+        [string] $username,
+        [string] $password,
+        [string] $domain,
+        [string] $url
+    )
+
+    $Credentials = New-Object psobject -Property @{
+        username = $username
+        password = $password
+        domain = $domain
+    }
+
+    return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+}
+
+function Close-HRConnection(){
+    param(
+        $accessToken,
+        $url
+    )
+    return Invoke-RestMethod -Method post -uri "$url/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+}
+
+$accessToken = Open-HRConnection -username $username -password $UnsecurePassword -domain $Domain -url $url
+
+$pools = Invoke-RestMethod -Method Get -uri "$url/rest/inventory/v5/desktop-pools" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$poolid = ($pools | where-object {$_.name -eq "vmug_demo_pool2"}).id
+$pool = Invoke-RestMethod -Method Get -uri "$url/rest/inventory/v5/desktop-pools/$poolid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$pool.display_protocol_settings.psobject.properties.remove("display_protocols")
+$pool.display_protocol_settings.psobject.properties.remove("html_access_enabled")
+$pool.display_protocol_settings.psobject.properties.remove("grid_vgpus_enabled")
+$pool.view_storage_accelerator_settings.psobject.properties.remove("add_virtual_tpm")
+$pool.view_storage_accelerator_settings.psobject.properties.remove("view_storage_accelerator_disk_types")
+$pool.view_storage_accelerator_settings.psobject.properties.remove("regenerate_view_storage_accelerator_days")
+$pool.provisioning_settings.psobject.properties.remove("min_ready_vms_on_vcomposer_maintenance")
+$pool.provisioning_settings.psobject.properties.remove("add_virtual_tpm")
+$pool.provisioning_settings.psobject.properties.remove("parent_vm_id")
+$pool.provisioning_settings.psobject.properties.remove("base_snapshot_id")
+$pool.provisioning_settings.psobject.properties.remove("datacenter_id")
+$pool.provisioning_settings.psobject.properties.remove("vm_folder_id")
+$pool.storage_settings.datastores | foreach {$_.psobject.properties.remove("storage_overcommit")}
+$pool.storage_settings.datastores | foreach {$_.psobject.properties.remove("sdrs_cluster")}
+$pool.storage_settings.psobject.properties.remove("use_native_snapshots")
+$pool.storage_settings.psobject.properties.remove("redirect_windows_profile")
+$pool.storage_settings.psobject.properties.remove("use_separate_datastores_persistent_and_os_disks")
+$pool.storage_settings.psobject.properties.remove("non_persistent_redirect_disposable_files")
+$pool.storage_settings.psobject.properties.remove("reclaim_vm_disk_space")
+$pool.session_settings.psobject.properties.remove("refresh_os_disk_after_logoff")
+
+$json = $pool | select-object -excludeproperty allow_rds_pool_multi_session_per_user,automatic_user_assignment,id,name,delete_in_progress,type,source,image_source,vcenter_id,user_assignment,provisioning_status_data,user_group_count,naming_method |  ConvertTo-Json -Depth 100
+Invoke-RestMethod -Method Put -uri "$url/rest/inventory/v1/desktop-pools/$poolid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken) -body $json
