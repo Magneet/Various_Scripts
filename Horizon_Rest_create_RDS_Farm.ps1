a24cad20beb53b2ee2d72e4ad2e64750eab91b32
Wouter Kursten <wouter@retouw.nl>
2022-03-17 20:04:17 +0100

    17032022

---
+++ Horizon_Rest_create_RDS_Farm.ps1
@@ -0,0 +1,279 @@
+<#
+    .SYNOPSIS
+    Creates a new Golden Image to a RDS Farm
+
+    .DESCRIPTION
+    This script uses the Horizon rest api's to create a new VMware Horizon RDS Farm
+
+    .EXAMPLE
+    Horizon_Rest_create_RDS_Farm.ps1 -Credentials $creds -ConnectionServerURL https://pod1cbr1.loft.lab -jsonfile 'D:\homelab\new-pool-rest.json' -vCenterURL pod1vcr1.loft.lab -DataCenterName "Datacenter_Loft" -ClusterName "Dell 620" -BaseVMName "W21h1-2021-11-05-13-00" -BaseSnapShotName "Created by Packer" -DatastoreNames  ("vdi-200","vdi-500") -VMFolderPath "/Datacenter_Loft/vm" -DesktopPoolName "Rest_Pool_demo2" -DesktopPoolDisplayName "Rest DIsplay name" -DesktopPoolDescription "rest description" -namingmethod "Rest-{n:fixed=2}" -ADOUrdn "OU=Pool01,OU=VDI,OU=Pod1,OU=VMware,OU=EUC"
+
+    .PARAMETER Credential
+    Mandatory: No
+    Type: PSCredential
+    Object with credentials for the connection server with domain\username and password. If not supplied the script will ask for user and password.
+
+    .PARAMETER ConnectionServerURL
+    Mandatory: Yes
+    Default: String
+    URL of the connection server to connect to
+
+    .PARAMETER vCenterURL
+    Mandatory: Yes
+    Username of the user to look for
+
+    .PARAMETER DataCenterName
+    Mandatory: Yes
+    Name of the datacenter
+
+    .PARAMETER BaseVMName
+    Mandatory: Yes
+    Name of the Golden Image VM
+
+    .PARAMETER BaseSnapShotName
+    Mandatory: Yes
+    Name of the Golden Image Snapshot
+
+    .PARAMETER RDSFarmName
+    Mandatory: Yes
+    Name of the RDS Farm to ctreate
+
+    .PARAMETER jsonfile
+    Mandatory: Yes
+    Full path to the JSON file to use as base
+
+    .PARAMETER ClusterName
+    Mandatory: Yes
+    Name of the vCenter Cluster to place the vm's in
+
+    .PARAMETER DatastoreNames
+    Mandatory: Yes
+    Array of names of the datastores to use
+
+    .PARAMETER VMFolderPath
+    Mandatory: Yes
+    Path to the folder where the folder with pool vm's will be placed including the datacenter with forward slashes so /datacenter/folder
+
+    .PARAMETER RDSFarmDisplayName
+    Mandatory: Yes
+    Display name of the RDS Farm
+
+    .PARAMETER RDSFarmDescription
+    Mandatory: Yes
+    Description of the RDS Farm
+
+    .PARAMETER NamingMethod
+    Mandatory: Yes
+    Naming method of the vm's
+
+    .PARAMETER ADOUrdn
+    Mandatory: Yes
+    Relative Distinguished Name for the OU where the vm's will be placed
+
+    .NOTES
+    Minimum required version: VMware Horizon 8 2106
+    Created by: Wouter Kursten
+    First version: 25-02-2022
+
+    .COMPONENT
+    Powershell Core
+#>
+
+<#
+Copyright © 2021 Wouter Kursten
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false,
+    HelpMessage='Credential object as domain\username with password' )]
+    [PSCredential] $Credentials,
+
+    [Parameter(Mandatory=$true,  HelpMessage='FQDN of the connectionserver' )]
+    [ValidateNotNullOrEmpty()]
+    [string] $ConnectionServerURL,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "URL of the vCenter to look in i.e. https://vcenter.domain.lab")]
+    [ValidateNotNullOrEmpty()]
+    [string]$vCenterURL,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Datacenter to look in.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$DataCenterName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Golden Image VM.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$BaseVMName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the Snapshot to use for the Golden Image.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$BaseSnapShotName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the RDS Farm.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$RDSFarmName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Display Name of the RDS Farm.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$RDSFarmDisplayName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Description of the RDS Farm.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$RDSFarmDescription,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Name of the cluster where the RDS Farm will be placed.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$ClusterName,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Array of names for the datastores where the Desktop will be placed.")]
+    [ValidateNotNullOrEmpty()]
+    [array]$DatastoreNames,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Path to the folder where the folder for the RDS Farm will be placed i.e. /Datacenter_Loft/vm")]
+    [ValidateNotNullOrEmpty()]
+    [string]$VMFolderPath,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Relative Distinguished Name for the OU where the vm's will be placed i.e. OU=Pool01,OU=VDI,OU=Pod1,OU=VMware,OU=EUC")]
+    [ValidateNotNullOrEmpty()]
+    [string]$ADOUrdn,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Naming method for the VDI machines.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$NamingMethod,
+
+    [parameter(Mandatory = $true,
+    HelpMessage = "Full path to the Json with RDS Farm details.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$jsonfile
+)
+
+try{
+    test-path $jsonfile  | out-null
+}
+catch{
+    throw "Json file not found"
+}
+try{
+    $sourcejson = get-content $jsonfile | ConvertFrom-Json
+}
+catch{
+    throw "Error importing json file"
+}
+
+if($Credentials){
+    $username=($credentials.username).split("\")[1]
+    $domain=($credentials.username).split("\")[0]
+    $password=$credentials.password
+}
+else{
+    $credentials = Get-Credential
+    $username=($credentials.username).split("\")[1]
+    $domain=($credentials.username).split("\")[0]
+    $password=$credentials.password
+}
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password) 
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+function Get-HRHeader(){
+    param($accessToken)
+    return @{
+        'Authorization' = 'Bearer ' + $($accessToken.access_token)
+        'Content-Type' = "application/json"
+    }
+}
+function Open-HRConnection(){
+    param(
+        [string] $username,
+        [string] $password,
+        [string] $domain,
+        [string] $url
+    )
+
+    $Credentials = New-Object psobject -Property @{
+        username = $username
+        password = $password
+        domain = $domain
+    }
+
+    return invoke-restmethod -Method Post -uri "$ConnectionServerURL/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+}
+function Close-HRConnection(){
+    param(
+        $accessToken,
+        $ConnectionServerURL
+    )
+    return Invoke-RestMethod -Method post -uri "$ConnectionServerURL/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+}
+
+try{
+    $accessToken = Open-HRConnection -username $username -password $UnsecurePassword -domain $Domain -url $ConnectionServerURL
+}
+catch{
+    throw "Error Connecting: $_"
+}
+
+$vCenters = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/monitor/v2/virtual-centers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$vcenterid = ($vCenters | where-object {$_.name -like "*$vCenterURL*"}).id
+$datacenters = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/datacenters?vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$datacenterid = ($datacenters | where-object {$_.name -eq $DataCenterName}).id
+$clusters = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/hosts-or-clusters?datacenter_id=$datacenterid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$clusterid = ($clusters | where-object {$_.details.name -eq $ClusterName}).id
+$datastores = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/datastores?host_or_cluster_id=$clusterid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$datastoreobjects = @()
+foreach ($datastoreName in $DatastoreNames){
+    $datastoreid = ($datastores | where-object {$_.name -eq $datastoreName}).id
+    [PSCustomObject]$dsobject=[ordered]@{
+        datastore_id = $datastoreid
+    }
+    $datastoreobjects+=$dsobject
+
+}
+$resourcepools = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/resource-pools?host_or_cluster_id=$clusterid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$resourcepoolid = $resourcepools[0].id
+$vmfolders = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/vm-folders?datacenter_id=$datacenterid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$vmfolderid = ($vmfolders | where-object {$_.path -eq $VMFolderPath}).id
+$basevms = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/base-vms?datacenter_id=$datacenterid&filter_incompatible_vms=false&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$basevmid = ($basevms | where-object {$_.name -eq $baseVMName}).id
+$basesnapshots = Invoke-RestMethod -Method Get -uri "$ConnectionServerURL/rest/external/v1/base-snapshots?base_vm_id=$basevmid&vcenter_id=$vcenterid" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+$basesnapshotid = ($basesnapshots | where-object {$_.name -eq $BaseSnapShotName}).id
+$sourcejson.provisioning_settings.base_snapshot_id = $basesnapshotid
+$sourcejson.provisioning_settings.datacenter_id = $datacenterid
+$sourcejson.provisioning_settings.host_or_cluster_id = $clusterid
+$sourcejson.provisioning_settings.parent_vm_id = $basevmid
+$sourcejson.provisioning_settings.vm_folder_id = $vmfolderid
+$sourcejson.provisioning_settings.resource_pool_id = $resourcepoolid
+$sourcejson.vcenter_id = $vcenterid
+$sourcejson.storage_settings.datastores = $datastoreobjects
+$sourcejson.name = $DesktopPoolName
+$sourcejson.display_name = $DesktopPoolDisplayName
+$sourcejson.description = $DesktopPoolDescription
+$sourcejson.pattern_naming_settings.naming_pattern = $namingmethod
+
+$json = $sourcejson | convertto-json -Depth 100
+
+try{
+    Invoke-RestMethod -Method Post -uri "$ConnectionServerURL/rest/inventory/v1/farms" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken) -body $json
+}
+catch{
+    throw $_
+}
---
+++ Horizon_send_Global_messages.ps1
@@ -0,0 +1,312 @@
+<#
+    .SYNOPSIS
+    Send a message to all user sessions
+
+    .DESCRIPTION
+    This script uses the Horizon rest api's to all sessions in a horizon pod
+
+    .EXAMPLE
+    .\find_user_assigned_desktops.ps1 -Credential $creds -ConnectionServerFQDN pod2cbr1.loft.lab -message "test message" -message_type "ERROR"
+
+    .PARAMETER Credential
+    Mandatory: No
+    Type: PSCredential
+    Object with credentials for the connection server with domain\username and password
+
+    .PARAMETER ConnectionServerFQDN
+    Mandatory: Yes
+    Default: String
+    FQDN of the connection server to connect to
+
+    .PARAMETER message
+    Mandatory: Yes
+    Message to send to the users
+
+    .PARAMETER message_type
+    Mandatory: Yes
+    Message type: INFO, ERROR or WARNING
+
+    .NOTES
+    Created by: Wouter Kursten
+    First version: 23-12-2021
+
+    .COMPONENT
+    Powershell Core
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false,
+    HelpMessage='Credential object as domain\username with password' )]
+    [PSCredential] $Credential,
+
+    [Parameter(Mandatory=$true,  HelpMessage='FQDN of the connectionserver' )]
+    [ValidateNotNullOrEmpty()]
+    [string] $ConnectionServerFQDN,
+
+    [Parameter(Mandatory=$true,
+    HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+    [string] $Message,
+
+    [Parameter(Mandatory=$true,
+    HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+    [validateset("ERROR","WARNING","INFO", IgnoreCase = $false)]
+    [string] $Message_Type
+)
+
+function Get-HRHeader(){
+    param($accessToken)
+    return @{
+        'Authorization' = 'Bearer ' + $($accessToken.access_token)
+        'Content-Type' = "application/json"
+    }
+}
+function Open-HRConnection(){
+    param(
+        [string] $username,
+        [string] $password,
+        [string] $domain,
+        [string] $url
+    )
+
+    $Credentials = New-Object psobject -Property @{
+        username = $username
+        password = $password
+        domain = $domain
+    }
+
+    return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+}
+
+function Close-HRConnection(){
+    param(
+        $accessToken,
+        $url
+    )
+    return Invoke-RestMethod -Method post -uri "$url/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+}
+
+function Get-HorizonRestData(){
+    [CmdletBinding(DefaultParametersetName='None')] 
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $ServerURL,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Array of ordered hashtables' )]
+        [array] $filters,
+
+        [Parameter(Mandatory=$true,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Type of filter Options: And, Or' )]
+        [ValidateSet('And','Or')]
+        [string] $Filtertype,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='Page size, default = 500' )]
+        [int] $pagesize = 500,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /external/v1/ad-users-or-groups' )]
+        [string] $RestMethod,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /external/v1/ad-users-or-groups' )]
+        [PSCustomObject] $accessToken,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="filteringandpagination",
+        HelpMessage='$True for rest methods that contain pagination and filtering, default = False' )]
+        [switch] $filteringandpagination,
+
+        [Parameter(Mandatory=$false,
+        ParameterSetName="id",
+        HelpMessage='To be used with single id based queries like /monitor/v1/connection-servers/{id}' )]
+        [string] $id,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage='Extra additions to the query url that comes before the paging/filtering parts like brokering_pod_id=806ca in /rest/inventory/v1/global-sessions?brokering_pod_id=806ca&page=2&size=100' )]
+        [string] $urldetails
+    )
+    
+    if($filteringandpagination){
+        if ($filters){
+            $filterhashtable = [ordered]@{}
+            $filterhashtable.add('type',$filtertype)
+            $filterhashtable.filters = @()
+            foreach($filter in $filters){
+                $filterhashtable.filters+=$filter
+            }
+            $filterflat=$filterhashtable | convertto-json -Compress
+            if($urldetails){
+                $urlstart= $ServerURL+"/rest/"+$RestMethod+"?"+$urldetails+"&filter="+$filterflat+"&page="
+            }
+            else{
+                $urlstart= $ServerURL+"/rest/"+$RestMethod+"?filter="+$filterflat+"&page="
+            }
+        }
+        else{
+            if($urldetails){
+                $urlstart= $ServerURL+"/rest/"+$RestMethod+"?"+$urldetails+"&page="
+            }
+            else{
+                $urlstart= $ServerURL+"/rest/"+$RestMethod+"?page="
+            }
+        }
+        $results = [System.Collections.ArrayList]@()
+        $page = 1
+        $uri = $urlstart+$page+"&size=$pagesize"
+        write-host $uri
+        $response = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+        $response.foreach({$results.add($_)}) | out-null
+        if ($responseheader.HAS_MORE_RECORDS -contains "TRUE") {
+            do {
+                $page++
+                $uri = $urlstart+$page+"&size=$pagesize"
+                $response = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+                $response.foreach({$results.add($_)}) | out-null
+            } until ($responseheader.HAS_MORE_RECORDS -notcontains "TRUE")
+        }
+    }
+    elseif($id){
+        $uri= $ServerURL+"/rest/"+$RestMethod+"/"+$id
+        $results = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+    }
+    else{
+        if($urldetails){
+            $uri= $ServerURL+"/rest/"+$RestMethod+"?"+$urldetails
+        }
+        else{
+            $uri= $ServerURL+"/rest/"+$RestMethod
+        }
+        
+        $results = Invoke-RestMethod $uri -Method 'GET' -Headers (Get-HRHeader -accessToken $accessToken) -ResponseHeadersVariable responseheader
+    }
+
+    return $results
+}
+
+function get-horizonglobalsessions(){
+    [CmdletBinding(DefaultParametersetName='None')] 
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $ServerURL,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /external/v1/ad-users-or-groups' )]
+        [PSCustomObject] $accessToken,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Id of the Local pod to query' )]
+        [string] $podid
+    )
+    try{
+        $results=Get-HorizonRestData -ServerURL $url -RestMethod "/inventory/v1/global-sessions" -accessToken $accessToken -urldetails "pod_id=$podid"
+    }
+    catch{
+        throw $_
+    }
+    return $results
+}
+
+function Get-Pods(){
+    [CmdletBinding(DefaultParametersetName='None')] 
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $ServerURL,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /external/v1/ad-users-or-groups' )]
+        [PSCustomObject] $accessToken
+    )
+
+    try{
+        $results=Get-HorizonRestData -ServerURL $url -RestMethod "/federation/v1/pods" -accessToken $accessToken
+    }
+    catch{
+        throw $_
+    }
+    return $results
+}
+
+function send-horizonglobalmessage(){
+    [CmdletBinding(DefaultParametersetName='None')] 
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $ServerURL,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Part after the url in the swagger UI i.e. /external/v1/ad-users-or-groups' )]
+        [PSCustomObject] $accessToken,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [array] $Session_Ids,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [string] $Message,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='url to the server i.e. https://pod1cbr1.loft.lab' )]
+        [validateset("ERROR","WARNING","INFO", IgnoreCase = $false)]
+        [string] $Message_Type,
+
+        [Parameter(Mandatory=$true,
+        HelpMessage='Id of the Local pod to query' )]
+        [string] $podid
+    )
+
+    $jsonhashtable = [ordered]@{}
+    $jsonhashtable.global_session_action_specs=@()
+    $sessiondetailshashtable = [ordered]@{}
+    $sessiondetailshashtable.ids=$Session_Ids
+    $sessiondetailshashtable.pod_id=$podid
+    $jsonhashtable.global_session_action_specs+=$sessiondetailshashtable
+    $jsonhashtable.message = $message
+    $jsonhashtable.message_type = $Message_Type
+    $json = $jsonhashtable | convertto-json -depth 100
+
+    try{
+        $results = Invoke-RestMethod -Method Post -uri "$ServerURL/rest/inventory/v1/global-sessions/action/send-message" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken) -body $json
+    }
+    catch{
+        throw $_
+    }
+    return $results
+}
+
+if($Credential){
+    $creds = $credential
+}
+else{
+    $creds = get-credential
+}
+
+$ErrorActionPreference = 'Stop'
+
+$username=($creds.username).split("\")[1]
+$domain=($creds.username).split("\")[0]
+$password=$creds.password
+
+$url = "https://$ConnectionServerFQDN"
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password) 
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$accessToken = Open-HRConnection -username $username -password $UnsecurePassword -domain $Domain -url $url
+
+$pods=get-pods -accessToken $accessToken -ServerURL $url
+
+foreach($pod in $pods){
+    $podid=$pod.id
+    $sessions = get-horizonglobalsessions -accessToken $accessToken -ServerURL $url -podid $podid
+    send-horizonglobalmessage -accessToken $accessToken -ServerURL $url -Message_Type $Message_Type -message $message -Session_Ids ($sessions).id -podid $podid
+}
